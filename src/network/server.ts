76149ff21856dcd1ce7597626af274af932f0986
banicaman <84487659+banicaman@users.noreply.github.com>
2022-07-10 23:49:15 -0500

    env port

--- src/network/server.ts
+++ src/network/server.ts
@@ -1,23 +1,23 @@
-import express from 'express';
-import morgan from 'morgan';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from 'express'
+import morgan from 'morgan'
+import mongoose from 'mongoose'
+import cors from 'cors'
 import { applyRoutes } from './router'
-const PORT = 3000;
+const PORT = 3000
 
-class Server{
-  private _app: express.Application;
+class Server {
+  private _app: express.Application
 
-  constructor() { 
-    this._app = express();
-    this.config();
-    this.routes();
+  constructor() {
+    this._app = express()
+    this.config()
+    this.routes()
   }
 
   private config() {
-    this._app.use(morgan('dev'));
-    this._app.use(express.json());
-    this._app.use(express.urlencoded({ extended   : false }));
+    this._app.use(morgan('dev'))
+    this._app.use(express.json())
+    this._app.use(express.urlencoded({ extended: false }))
     this._app.use(cors())
     this._app.use(
       (
@@ -37,19 +37,19 @@ class Server{
     applyRoutes(this._app)
   }
 
-  private routes(){
-    this._app.get('/', (req, res) => {res.send("Server2")});
+  private routes() {
+    this._app.get('/', (req, res) => {
+      res.send('Server2')
+    })
   }
 
   private async _sequelize(): Promise<void> {
     try {
-        const uri = "mongodb+srv://Hasser:v4quVbwB7TDLN@cluster0.4jglo.mongodb.net/?retryWrites=true&w=majority";
-        // Connect to the MongoDB cluster
-        mongoose.connect(
-            uri,
-          () => console.log(" Mongoose is connected")
-        );
-    
+      const uri =
+        'mongodb+srv://Hasser:v4quVbwB7TDLN@cluster0.4jglo.mongodb.net/?retryWrites=true&w=majority'
+      // Connect to the MongoDB cluster
+      mongoose.connect(uri, () => console.log(' Mongoose is connected'))
+
       // console.log('Database connection established')
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (e: any) {
@@ -59,7 +59,7 @@ class Server{
   }
 
   public start(): void {
-    this._app.listen(PORT, () => {
+    this._app.listen(PORT || process.env.PORT, () => {
       console.log(`Server running at port ${PORT}`)
     })
     try {
@@ -68,9 +68,7 @@ class Server{
       console.error(e)
     }
   }
-
 }
 const server = new Server()
-
 
 export { server as Server }
